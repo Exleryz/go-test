bf1fb8535275cfb4342478e03304c12e60e05af0
Exleryz <zyezhou@163.com>
2019-08-21 15:58:26 +0800

    go 读取配置文件 默认.env

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ go 1.12
 require (
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
-	github.com/thinkerou/favicon v0.1.0
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"go-test/server"
 )
 
 func main() {
+	// 读取配置文文件
+	godotenv.Load()
 	r := server.Router()
 	// 默认启动方式，包含 Logger、Recovery 中间件
 	//r.Run(":8080")
